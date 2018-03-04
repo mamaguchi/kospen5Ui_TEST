98a924efeb2911d3e0ceece3cdfd0880aa56a95f
patrick <patrickchow688@gmail.com>
2018-03-05 06:38:30 +0800

    5March2018 - Added App Bar(toolbar) to MainActivity.

--- app/src/main/java/com/example/patrick/kospen5ui_test/MainActivity.java
+++ app/src/main/java/com/example/patrick/kospen5ui_test/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.patrick.kospen5ui_test;
 
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 import  com.example.patrick.kospen5ui_test.fragments.FragmentOne;
 import  com.example.patrick.kospen5ui_test.fragments.FragmentThree;
@@ -22,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
         ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new FragmentOne(), "FRAG1");
@@ -34,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
 
 
     // Adapter for the viewpager using FragmentPagerAdapter
@@ -66,4 +72,36 @@ public class MainActivity extends AppCompatActivity {
             return mFragmentTitleList.get(position);
         }
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate our menu from the resources by using the menu inflater.
+        getMenuInflater().inflate(R.menu.main, menu);
+        //should return true if you have added items to it and want the menu to be displayed.
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+
 }
